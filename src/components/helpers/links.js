9ff8a8631e169ac9aa8271fb87fb77c930639c5a
Igor <iigorfelipe@gmail.com>
2022-06-09 23:15:11 -0300

    adiciona novo background e remove o indicator junto com seu css

---
+++ src/components/helpers/links.js
@@ -0,0 +1,35 @@
+import { AiOutlineHome } from 'react-icons/ai';
+import { CgProfile } from 'react-icons/cg';
+import { TiMessages } from 'react-icons/ti';
+import { MdOutlinePhotoSizeSelectActual } from 'react-icons/md';
+
+export const arrayLinks = [
+  {
+    id: 1,
+    to: "/",
+    name: "Home",
+    icon: <AiOutlineHome />,
+    className: "list"
+  },
+  {
+    id: 2,
+    to: "/about",
+    name: "About",
+    icon: <CgProfile />,
+    className: "list"
+  },
+  {
+    id: 3,
+    to: "/portfolio",
+    name: "Portfolio",
+    icon: <MdOutlinePhotoSizeSelectActual />,
+    className: "list"
+  },
+  {
+    id: 4,
+    to: "/contact",
+    name: "Contact",
+    icon: <TiMessages />,
+    className: "list"
+  }
+];
