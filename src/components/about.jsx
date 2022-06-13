df80eeb221c7350b5acf4d9186caef638263ced9
Igor <iigorfelipe@gmail.com>
2022-06-12 22:18:14 -0300

    atualiza o about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,8 +11,11 @@ export default function About() {
 
       <div className="about-container">
         <div className="about-about">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, obcaecati iste et voluptates, cum omnis qui totam sequi in placeat nulla inventore, maxime quibusdam quam! Ut repellendus illo saepe vero?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, obcaecati iste et voluptates, cum omnis qui totam sequi in placeat nulla inventore, maxime quibusdam quam! Ut repellendus illo saepe vero?</p>
+          <p>Olá, meu nome é Igor, sou estudante de desenvolvimento web, comecei pelo YouTube em Julho de 2021 só para ver se eu ia gostar, já que eu amo <span>tecnologia</span>, e não deu outra, não só gostei como me apaixonei.</p>
+          <br />
+          <p>Além te ter gostado muito eu via <span>futuro</span> nisso, decidi então investir tudo nessa área, foi quando entrei na Trybe como estudante e foi a melhor escolha da minha vida.</p>
+          <br />
+          <p>Para me dedicar 100% nos estudos, saí do meu antigo emprego e nessa jornada adquiri muitos conhecimentos tanto no <span>Front-end</span> quanto no <span>Back-end</span>, eu amei ambas as áreas e por isso estou em busca de me tornar um desenvolvedor <span>Web Full Stack</span>.</p>
         </div>
 
         <div className="about-skills">
